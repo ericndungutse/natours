95c325c11c4991bddb49d7fdb2a5004251ffcaf0
Eric Tuyizere NDUNGUTSE <dav.ndungutse@gmail.com>
2021-08-04 16:52:07 +0200

    Setting up compression

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errController');
@@ -74,6 +75,10 @@ app.use(
     ],
   })
 );
+
+// Compress all text/responses sent to client Except images coz
+// They are already compressed
+app.use(compression());
 
 // ROUTES
 app.use('/', viewRouter);
